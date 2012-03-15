e27736b3765eb954553ccaeca0d781f1e0b41261
jsloan <jsloan@168d8bee-89bf-453e-b8b5-b1230d3c0dc2>
2012-03-15 15:44:30 +0000

    Checkpoint after adding avr-size to Makefile.
    git-svn-id: svn://graphite/amigo/trunk/Amigo@29 168d8bee-89bf-453e-b8b5-b1230d3c0dc2

--- Amigo/Demo/EtherMega2560_GCC/MegaBlink/main.c
+++ Amigo/Demo/EtherMega2560_GCC/MegaBlink/main.c
@@ -23,6 +23,8 @@
 /* serial interface include file. */
 #include <lib_serial.h>
 
+#include <stdint.h> // < coverclock 2012-03-14
+
 
 /*-----------------------------------------------------------*/
 /* Create a handle for the serial port. */
@@ -39,14 +41,18 @@ static portTASK_FUNCTION(TaskBlinkGreenLED, pvParameters); // Main Arduino Uno 3
 int main(void)
 {
 	// v coverclock@diag.com 2012-03-14
-	MCUCR &= ~_BV(JTD); // Enable JTAG debugging.
-	MCUCR &= ~_BV(IVSEL); // Take interrupt vectors.
+	uint8_t mcusr = MCUSR;
+	MCUSR = 0x0;
+	MCUCR &= ~( _BV(JTD) | _BV(IVSEL) ); // Enable JTAG debugging. Take interrupt vectors.
+	MCUSR &= ~( _BV(WDRF) ); // Reset Watchdog.
+	WDTCSR |= ( _BV(WDCE) | _BV(WDE) ); // Enable Watchdog Change. Disable Watchdog Reset.
+	WDTCSR = 0x0; // Disable Watchdog.
 	// ^ coverclock@diag.com 2012-03-14
 
     // turn on the serial port for debugging or for other USART reasons.
 	xSerialPort = xSerialPortInitMinimal( 115200, 80, 16); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
 
-	avrSerialPrint_P(PSTR("\r\n\n\nHello World!\r\n")); // Ok, so we're alive...
+	avrSerialPrintf_P(PSTR("\r\n\n\nHello World! 0x%x\r\n"), mcusr); // Ok, so we're alive...
 
 #if 1
     xTaskCreate(
@@ -68,7 +74,7 @@ int main(void)
 		,  NULL ); // */
 #endif
 
-	avrSerialPrintf_P(PSTR("\r\n\nFree Heap Size: %u\r\n"),xPortGetFreeHeapSize() ); // needs heap_1 or heap_2 for this function to succeed.
+	avrSerialPrintf_P(PSTR("\r\n\nFree Heap Size: %u\r\n"), xPortGetFreeHeapSize() ); // needs heap_1 or heap_2 for this function to succeed.
 
     vTaskStartScheduler();
 
@@ -95,18 +101,17 @@ static void TaskBlinkRedLED(void *pvParameters) // Main Red LED Flash
     {
 
     	setDigitalOutput( IO_B7, 1);               // main EtherMega LED on
-		vTaskDelayUntil( &xLastWakeTime, ( 1000 / portTICK_RATE_MS ) ); // < coverclock@diag.com 2012-03-12
+		vTaskDelayUntil( &xLastWakeTime, ( 1000 / portTICK_RATE_MS ) ); // < coverclock@diag.com 2012-03-14
 
 		setDigitalOutput( IO_B7, 0);               // main EtherMega LED  off
-		vTaskDelayUntil( &xLastWakeTime, ( 2000 / portTICK_RATE_MS ) ); // < coverclock@diag.com 2012-03-12
+		vTaskDelayUntil( &xLastWakeTime, ( 4000 / portTICK_RATE_MS ) ); // < coverclock@diag.com 2012-03-14
 
-#if 0
 		xSerialPrintf_P(PSTR("RedLED HighWater @ %u\r\n"), uxTaskGetStackHighWaterMark(NULL));
-#endif
     }
 
 }
 
+#if 0
 /*-----------------------------------------------------------*/
 static void TaskBlinkGreenLED(void *pvParameters) // Main Green LED Flash
 {
@@ -134,4 +139,66 @@ static void TaskBlinkGreenLED(void *pvParameters) // Main Green LED Flash
 
 }
 /*-----------------------------------------------------------*/
+#endif
 
+#if 1
+#include <avr/interrupt.h>
+static volatile uint64_t fake_isr = 0;
+#define FAKE_ISR(_N_) ISR(_VECTOR(_N_)) { fake_isr |= (1ULL << _N_); }
+FAKE_ISR(1)
+FAKE_ISR(2)
+FAKE_ISR(3)
+FAKE_ISR(4)
+FAKE_ISR(5)
+FAKE_ISR(6)
+FAKE_ISR(7)
+FAKE_ISR(8)
+FAKE_ISR(9)
+FAKE_ISR(10)
+FAKE_ISR(11)
+FAKE_ISR(12)
+FAKE_ISR(13)
+FAKE_ISR(14)
+FAKE_ISR(15)
+FAKE_ISR(16)
+FAKE_ISR(17)
+FAKE_ISR(18)
+FAKE_ISR(19)
+FAKE_ISR(20)
+FAKE_ISR(21)
+FAKE_ISR(22)
+FAKE_ISR(23)
+FAKE_ISR(24)
+//FAKE_ISR(25)
+//FAKE_ISR(26)
+FAKE_ISR(27)
+FAKE_ISR(28)
+FAKE_ISR(29)
+FAKE_ISR(30)
+FAKE_ISR(31)
+//FAKE_ISR(32)
+FAKE_ISR(33)
+FAKE_ISR(34)
+FAKE_ISR(35)
+FAKE_ISR(36)
+FAKE_ISR(37)
+FAKE_ISR(38)
+FAKE_ISR(39)
+FAKE_ISR(40)
+FAKE_ISR(41)
+FAKE_ISR(42)
+FAKE_ISR(43)
+FAKE_ISR(44)
+FAKE_ISR(45)
+FAKE_ISR(46)
+FAKE_ISR(47)
+FAKE_ISR(48)
+FAKE_ISR(49)
+FAKE_ISR(50)
+FAKE_ISR(51)
+FAKE_ISR(52)
+FAKE_ISR(53)
+FAKE_ISR(54)
+FAKE_ISR(55)
+FAKE_ISR(56)
+#endif
