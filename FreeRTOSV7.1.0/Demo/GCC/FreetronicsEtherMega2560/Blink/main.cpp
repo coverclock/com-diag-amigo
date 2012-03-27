32d798bade4367210f0d06eee162ebadcf706686
jsloan <jsloan@168d8bee-89bf-453e-b8b5-b1230d3c0dc2>
2012-03-27 17:30:18 +0000

    Phase nine (seriously).
    git-svn-id: svn://graphite/amigo/trunk/Amigo@49 168d8bee-89bf-453e-b8b5-b1230d3c0dc2

---
+++ FreeRTOSV7.1.0/Demo/GCC/FreetronicsEtherMega2560/Blink/main.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file
+ * Copyright 2012 Digital Aggregates Corporation, Colorado, USA\n
+ * Licensed under the terms in README.h\n
+ * Chip Overclock mailto:coverclock@diag.com\n
+ * http://www.diag.com/navigation/downloads/Amigo.html\n
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "com/diag/amigo/Serial.h"
+
+int main(void)
+{
+	sei();
+
+	com::diag::amigo::Serial console;
+
+	console.start();
+
+	console.emit('r');
+	console.emit('u');
+	console.emit('n');
+	console.emit('n');
+	console.emit('i');
+	console.emit('n');
+	console.emit('g');
+	console.emit('\r');
+	console.emit('\n');
+
+	DDRB |= _BV(7);
+	for (int ii = 0; ii < 10; ++ii) {
+		PORTB &= ~_BV(7);
+		_delay_ms(500);
+		PORTB |= _BV(7);
+		_delay_ms(500);
+	}
+
+	console.write('r');
+	console.write('e');
+	console.write('a');
+	console.write('d');
+	console.write('y');
+	console.write('\r');
+	console.write('\n');
+
+	for (;;) {
+		while (console.available() > 0) {
+			console.write(console.read());
+		}
+	}
+
+    return(0);
+}
