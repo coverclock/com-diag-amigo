655892a17a0416e2e088bbab2561d696866e06c8
jsloan <jsloan@168d8bee-89bf-453e-b8b5-b1230d3c0dc2>
2012-03-27 17:33:55 +0000

    Phase, what, twelve?
    git-svn-id: svn://graphite/amigo/trunk@51 168d8bee-89bf-453e-b8b5-b1230d3c0dc2

---
+++ Amigo/FreeRTOSV7.1.0/Amigo/GCC/unused.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Copyright 2012 Digital Aggregates Corporation, Colorado, USA\n
+ * Licensed under the terms in README.h\n
+ * Chip Overclock mailto:coverclock@diag.com\n
+ * http://www.diag.com/navigation/downloads/Amigo.html\n
+ */
+
+#include "com/diag/amigo/unused.h"
+
+namespace com {
+namespace diag {
+namespace amigo {
+
+Unused unused;
+
+}
+}
+}
