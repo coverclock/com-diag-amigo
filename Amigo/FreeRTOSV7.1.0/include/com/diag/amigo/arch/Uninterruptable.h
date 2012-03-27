2d8e76275e4bbf5c32fb8b2050f6b959b41d0ae5
jsloan <jsloan@168d8bee-89bf-453e-b8b5-b1230d3c0dc2>
2012-03-27 17:59:04 +0000

    Phase fourteen.
    git-svn-id: svn://graphite/amigo/trunk@53 168d8bee-89bf-453e-b8b5-b1230d3c0dc2

---
+++ Amigo/FreeRTOSV7.1.0/include/com/diag/amigo/arch/Uninterruptable.h
@@ -0,0 +1,65 @@
+#ifndef _COM_DIAG_AMIGO_UNINTERRUPTABLE_H_
+#define _COM_DIAG_AMIGO_UNINTERRUPTABLE_H_
+
+/**
+ * @file
+ * Copyright 2012 Digital Aggregates Corporation, Colorado, USA\n
+ * Licensed under the terms in README.h\n
+ * Chip Overclock mailto:coverclock@diag.com\n
+ * http://www.diag.com/navigation/downloads/Amigo.html\n
+ * These symbols are in the global name space!\n
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "com/diag/amigo/types.h"
+
+namespace com {
+namespace diag {
+namespace amigo {
+
+class Uninterruptable {
+
+public:
+
+	Uninterruptable()
+	: sreg(SREG)
+	{
+		cli();
+	}
+
+	~Uninterruptable() {
+		SREG = sreg;
+	}
+
+	operator uint8_t() {
+		return sreg;
+	}
+
+protected:
+
+	uint8_t sreg;
+
+private:
+
+    /**
+     *  Copy constructor. POISONED.
+     *
+     *  @param that refers to an R-value object of this type.
+     */
+	Uninterruptable(const Uninterruptable& that);
+
+    /**
+     *  Assignment operator. POISONED.
+     *
+     *  @param that refers to an R-value object of this type.
+     */
+	Uninterruptable& operator=(const Uninterruptable& that);
+
+};
+
+}
+}
+}
+
+#endif /* _COM_DIAG_AMIGO_UNINTERRUPTABLE_H_ */
